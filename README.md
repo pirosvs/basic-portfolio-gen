99dc52efa1c9e561d2d2fc03dc60385d0c445767
darmstrong <armstrong2579@gmail.com>
2022-07-15 11:04:13 -0700

    add prompts and urls to grab (not placed correctly yet)

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # basic-portfolio-gen
+
+## Description
+Takes input from command line using inquirer and generates a basic html portfolio page
---
+++ index.js
@@ -0,0 +1,70 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+inquirer
+  .prompt({
+    message: "Enter your GitHub username",
+    name: "username"
+  })
+  .then(function({ github }) {
+    const githubUrl = `https://github.com/${github}`;
+});
+
+.then(function({ linkedin }) {
+    const linkedinUrl = `https://linkedin.com/${linkedin}`;
+});
+
+
+// * An HTML document containing the information collected from the prompts should be constructed and written to the
+// file system. Be sure to add some CSS styling to the document.
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'Where are you from?',
+        name: 'location',
+    },
+    {
+        type: 'input',
+        message: 'Please enter any information you would like to include in your bio.',
+        name: 'bio',
+    },
+    {
+        type: 'input',
+        message: 'What languages do you know?',
+        choices: ['HTML', 'CSS', 'JavaScript', 'Python', 'varying C languages'],
+        name: 'languages',
+    },
+    {
+        type: 'input',
+        message: 'What is your LinkdedIn username?',
+        name: 'linkedin',
+    },
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your preferred method of communication?',
+        choices: ['email', 'phone', 'mail', 'other'],
+        name: 'comms',
+    },
+  ])
+  .then((response) => {
+    console.log(response)
+  }
+  );
+
+  writeFile('portfolio.html', ourString, (err) => {
+    if (err) throw err;
+    console.log(`Congratulations! You have created a portfolio page.`);
+  });
+  
